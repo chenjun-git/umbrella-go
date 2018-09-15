aced197f1bd7fe57d8f9b62211466f241aabbdc1
chenjun <chenjun@meiqia.com>
2018-09-15 15:45:28 +0800

    add grpc lang

---
+++ umbrella-common/middleware/grpc/error_translator.go
@@ -0,0 +1,38 @@
+package grpc
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"umbrella-go/umbrella-common/lang"
+	proto "umbrella-go/proto"
+)
+
+type errorGetter interface {
+	GetError() *proto.Error
+}
+
+type ErrorMsgGetter func(code int, languages []string) string
+
+func MakeUnaryServerErrorTranslator(errorMsgGetter ErrorMsgGetter) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		languages := lang.FromIncomingContext(ctx)
+		ctx = lang.ContextSetLanguages(ctx, languages)
+		resp, err = handler(ctx, req)
+		if resp, ok := resp.(errorGetter); ok {
+			err := resp.GetError()
+
+			if err != nil {
+				// 设置err的message信息
+				if err.Message == "" {
+					if msg := errorMsgGetter(int(err.Code), languages); msg != "" {
+						err.Message = msg
+					} else {
+						err.Message = "Unknown error"
+					}
+				}
+			}
+		}
+		return resp, err
+	}
+}
